e087c5c3149a612977e6f4ca590fcc4c504b3da2
Uthmanbello <uthmanbello88@gmail.com>
2023-07-22 06:12:25 +0100

    Setup carousel

--- scripts/slideshow.js
+++ scripts/slideshow.js
@@ -1,45 +1,23 @@
-// const slideshow = document.querySelector('.slideshow');
-// const slides = document.querySelectorAll('.slideshow img');
-// const dots = document.querySelectorAll('.dot1');
-// let slideIndex = 0;
+$(document).ready(function () {
+  const slides = $('.slides img');
+  const dots = $('.dot');
+  let slideIndex = 0;
 
-// function showSlide(index) {
-//   slideshow.style.transform = `translateX(-${index * 100}%)`;
-//   dots.forEach((dot, i) => dot.classList.toggle('active', i === index));
-// }
+  function showSlide(index) {
+    slides.removeClass('active').eq(index).addClass('active');
+    dots.removeClass('active').eq(index).addClass('active');
+  }
 
-// function nextSlide() {
-//   slideIndex = (slideIndex + 1) % slides.length;
-//   showSlide(slideIndex);
-// }
+  function nextSlide() {
+    slideIndex = (slideIndex + 1) % slides.length;
+    showSlide(slideIndex);
+  }
 
-// function prevSlide() {
-//   slideIndex = (slideIndex - 1 + slides.length) % slides.length;
-//   showSlide(slideIndex);
-// }
+  dots.on('click', function () {
+    slideIndex = $(this).index();
+    showSlide(slideIndex);
+  });
 
-// dots.forEach((dot, i) => dot.addEventListener('click', () => showSlide(i)));
-
-// setInterval(nextSlide, 3000); // Change slide every 3 seconds (adjust as needed)
-// showSlide(slideIndex); // Show the first slide on page load
-
-
-const slideshow = document.querySelector('.slideshow');
-const slides = document.querySelectorAll('.slideshow img');
-const dots = document.querySelectorAll('.dot1');
-let slideIndex = 0;
-
-function showSlide(index) {
-  slideshow.style.transform = `translateX(-${index * 100}%)`;
-  dots.forEach((dot, i) => dot.classList.toggle('active', i === index));
-}
-
-function nextSlide() {
-  slideIndex = (slideIndex + 1) % slides.length;
+  setInterval(nextSlide, 3000);
   showSlide(slideIndex);
-}
-
-dots.forEach((dot, i) => dot.addEventListener('click', () => showSlide(i)));
-
-setInterval(nextSlide, 3000); // Change slide every 3 seconds (adjust as needed)
-showSlide(slideIndex); // Show the first slide on page load
+});
